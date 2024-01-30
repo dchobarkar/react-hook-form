1b26fe93d4cbadda8163a3a2d2fd8956e333c058
Darshan Chobarkar <dchobarkar@gmail.com>
2024-01-30 19:58:57 +0530

    add basic component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const RegisterForm = () => {
+  const { register, handleSubmit } = useForm();
+  const handleRegistration = (data) => console.log(data);
 
-export default App;
+  return (
+    <form onSubmit={handleSubmit(handleRegistration)}>
+      <div>
+        <label>Name</label>
+        <input name="name" {...register("name")} />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" {...register("email")} />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" {...register("password")} />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default RegisterForm;
