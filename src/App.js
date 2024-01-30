ffd0ca29c3dcca50bfd2d15802b6fe08c11d1a9e
Darshan Chobarkar <dchobarkar@gmail.com>
2024-01-30 20:10:20 +0530

    fix mirror bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-import "./App.css";
-
 const RegisterForm = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
   const handleRegistration = (data) => console.log(data);
   const handleError = (errors) => {};
 
   const registerOptions = {
     name: { required: "Name is required" },
-    email: { required: "Email is required." },
+    email: { required: "Email is required" },
     password: {
-      required: "Password is required.",
+      required: "Password is required",
       minLength: {
         value: 8,
-        message: "Password must have at least 8 characters.",
+        message: "Password must have at least 8 characters",
       },
     },
   };
@@ -33,32 +32,35 @@ const RegisterForm = () => {
           type="text"
           {...register("name", registerOptions.name)}
         />
-        <small className="text-dander">
+        <small className="text-danger">
           {errors?.name && errors.name.message}
         </small>
       </div>
+
       <div>
         <label>Email</label>
         <input
-          name="email"
           type="email"
+          name="email"
           {...register("email", registerOptions.email)}
         />
-        <small className="text-dander">
-          {errors?.name && errors.name.message}
+        <small className="text-danger">
+          {errors?.email && errors.email.message}
         </small>
       </div>
+
       <div>
         <label>Password</label>
         <input
-          name="password"
           type="password"
+          name="password"
           {...register("password", registerOptions.password)}
         />
-        <small className="text-dander">
-          {errors?.name && errors.name.message}
+        <small className="text-danger">
+          {errors?.password && errors.password.message}
         </small>
       </div>
+
       <button>Submit</button>
     </form>
   );
